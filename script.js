285059cfc38221e13cef88a092c126590f907c51
Mandisa Reed <mandisa.reed@gmail.com>
2020-06-16 11:47:44 -0400

    added instructions div and test quiz questions

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 //WHEN the page loads, there is a homepage that explains the instructions for the quiz.
-
 //I need a container to display the instructions text.
-var instructions = document.querySelector(".quiz");
+//INSTRUCTIONS: This is a multiple-choice quiz to test your basic coding knowledge. There are __ questions. Each time you answer a question correctly, you earn 1 point. Each time you choose an incorrect answer, you lose 10 seconds off the timer. At the end, you will be able to enter your initials to save your score. Click the button when ready!
+var instructions = document.querySelector(".instructions");
 instructions.textContent = "This is a multiple-choice quiz of 5 questions to test your basic coding knowledge. You have 60 seconds to complete the quiz. Each time you answer a question correctly, you move on to the next question and the timer will continue counting down at the same rate. If you answer a question INCORRECTLY, the timer will decrease by 15 seconds, continue counting down, and the next question will appear. The quiz is finished once you answer all questions or the timer runs out. Good luck!";
 
 //At the end of the instructions, there is a BUTTON that says "begin quiz"
-//INSTRUCTIONS: This is a multiple-choice quiz to test your basic coding knowledge. There are __ questions. Each time you answer a question correctly, you earn 1 point. Each time you choose an incorrect answer, you lose 10 seconds off the timer. At the end, you will be able to enter your initials to save your score. Click the button when ready!
+
 
 //BEFORE THE GAME BEGINS: the time is at 90 seconds (1000ms = 1s)
+//I'm going to need to include a setInterval() function here. It will need to run a function itself...
 
 //WHEN the user CLICKS the button, the timer begins counting down from 90.
-//For each question (there will be 5), there will be 4 choices from which the user can click on.
+//For each question (there will be 5), there will be 3 or 4 choices from which the user can click on.
+
+//I need to create an object that house the arrays of questions, answers, and actual answers.
+var quiz = [
+    {
+    questionOne: "What does a javascript file end in?",
+    answersOne: {
+        a: ".html", 
+        b: ".js",
+        c: ".css"
+    },
+    actualAnswer1: "b"
+    },
+    {
+    questionTwo: "A string is defined by a set of ______. ",
+    answersTwo: {
+        a: "curly brackets",
+        b: "parentheses",
+        c: "quotation marks"
+    },
+    actualAnswer2: "c"
+    }
+];
 
 //IF the user chooses the correct answer (only one per question), "CORRECT!" will be displayed in the window, THEN one point will be added to their score and they move on to the next question.
 //ELSE the user chooses an incorrect answer (three per question), "WRONG!" will be displayed in the window, THEN their score does not change. HOWEVER, 10 seconds will be deducted from the timer, and they move on to the next question.
